2ddd9fbbc102e30dd5ca6d09e96e1eb68f17b841
Ricky Esparza <RickyEsparza@Rickys-MacBook.local>
2022-12-12 16:15:38 -0700

    added connection betweeen database and server, added question prompt to user

--- server.js
+++ server.js
@@ -16,3 +16,49 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json);
 
+// Used to connect with database using SQL
+const db = mysql2.creeateConnect(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: process.env.DB_Password,
+        database: 'employees_db'
+    }
+);
+
+db.connect((err) => {
+    if (err) throw err;
+    else{
+        questions();
+    }
+});
+
+// Questions given to the user
+
+questions = () => {
+    inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'options',
+            message: 'What would you like to do?',
+            options: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+        }
+    ]).then((userChoice) => {
+        const { options } = userChoice;
+        if (options === 'View All Employees') {
+            allEmployess();
+        } else if (options === 'Add Employee') {
+            newEmployee();
+        } else if (options === 'Update Employee Role') {
+            updateRole();
+        } else if (options === 'View All Roles') {
+            allRoles();
+        } else if (options === 'Add Role') {
+            newRole();
+        } else if (options === 'View All Departments') {
+            allDepartments();
+        } else {
+            newDepartment();
+        }
+    });
+}
